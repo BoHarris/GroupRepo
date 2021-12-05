54ae2dadbd13972c6954fd79918df17bb029bcca
Ethan Jolles <erjolles@gmail.com>
2021-12-05 02:17:10 -0500

    Added Scanner and arguments.

--- GroupProject/src/Jolles.java
+++ GroupProject/src/Jolles.java
@@ -1,6 +1,9 @@
-
+import java.util.Scanner;
 public class Jolles {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name.");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
     }
 }
